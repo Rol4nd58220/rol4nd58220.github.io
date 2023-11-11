36fc1356b288a8203a7eb3d3f4e23dfc8c817ccc
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-12 01:12:58 +0800

    Loading_Changes

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var loadingText = document.getElementById('loading-text');
     var launchButton = document.getElementById('launch-button');
     var loadingSteps = [50, 75, 93, 100];
-    var currentStepIndex = 0;
+    var currentStepIndex = 50;
 
     var updateLoadingText = function(percent) {
         loadingText.textContent = percent + '% Loading...';
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Start the loading process
     progressLoading();
-    
+
     launchButton.addEventListener('click', function() {
         document.getElementById('loading-screen').style.display = 'none';
         // Initialize your 3D scene or other scripts here
