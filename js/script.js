bd4d2196ac7810fbf0665f544df3b46cf370f59b
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-12 01:17:00 +0800

    Loading_Changes

--- js/script.js
+++ js/script.js
@@ -3,29 +3,32 @@ document.addEventListener('DOMContentLoaded', function() {
     var loadingText = document.getElementById('loading-text');
     var launchButton = document.getElementById('launch-button');
     var loadingSteps = [50, 75, 93, 100];
-    var currentStepIndex = 50;
+    var currentStepIndex = 0;
 
     var updateLoadingText = function(percent) {
         loadingText.textContent = percent + '% Loading...';
     };
 
+    // Start with the first step (50%)
+    updateLoadingText(loadingSteps[currentStepIndex]);
+
     var progressLoading = function() {
-        var currentPercent = loadingSteps[currentStepIndex];
-
-        updateLoadingText(currentPercent);
-
         if (currentStepIndex < loadingSteps.length - 1) {
             currentStepIndex++;
-            // Delay between steps
+            updateLoadingText(loadingSteps[currentStepIndex]);
             setTimeout(progressLoading, 1000); // 1 second between each step
         } else {
-            loadingText.style.display = 'none';
-            launchButton.style.display = 'block';
+            // Handle the final step
+            updateLoadingText(loadingSteps[currentStepIndex]);
+            setTimeout(function() {
+                loadingText.style.display = 'none';
+                launchButton.style.display = 'block';
+            }, 1000); // Wait for a moment before showing the button
         }
     };
 
-    // Start the loading process
-    progressLoading();
+    // Progress to the next step after an initial delay
+    setTimeout(progressLoading, 1000);
 
     launchButton.addEventListener('click', function() {
         document.getElementById('loading-screen').style.display = 'none';
