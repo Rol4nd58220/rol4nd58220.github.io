a32f7477715d373c667388d344f7a43177f52c5c
Rol4nd58220 <rolandampalo5822@gmail.com>
2023-11-11 19:54:16 +0800

    loading_Screen_02

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("JavaScript is loaded!");
+document.addEventListener('DOMContentLoaded', function() {
+    var loadingText = document.getElementById('loading-text');
+    var percent = 0;
+
+    var loadingInterval = setInterval(function() {
+        percent += 1;
+        loadingText.textContent = percent + '%';
+
+        if (percent >= 100) {
+            clearInterval(loadingInterval);
+            loadingText.textContent = 'Launch';
+            setTimeout(function() {
+                document.getElementById('loading-screen').style.display = 'none';
+                // Initialize your 3D scene or other scripts here
+            }, 1000); // 1 second delay for 'Launch' message
+        }
+    }, 50); // Adjust time for simulation of loading
+});
